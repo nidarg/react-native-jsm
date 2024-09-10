471c5757275af06ac74e9729d550d01af4b15b08
Gradin <gradin_g@yahoo.com>
2024-09-10 06:15:48 +0300

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className='text-3xl'>Aora!</Text>
+      <StatusBar style="auto" />
+      <Link style={{color:'blue'}} href='/profile'>Go to profile</Link>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+ 
+// });
